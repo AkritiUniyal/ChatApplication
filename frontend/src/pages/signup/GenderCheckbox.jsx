51c1a9207e7a200fb9a321d8929a9e3ceaa59b4d
AkritiUniyal <akritiuniyal31@gmail.com>
2024-07-27 11:38:43 +0530

    change

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,7 +3,7 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 		<div className='flex'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
-					<span className='label-text'>Male</span>
+					<span className='label-text'style={{ color: '#1a237e', fontWeight: 'bold' }}>Male</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
@@ -14,9 +14,9 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 			</div>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
-					<span className='label-text'>Female</span>
+					<span className='label-text'style={{ color: '#1a237e', fontWeight: 'bold' }}>Female</span>
 					<input
-						type='checkbox'
+						type='checkbox' 
 						className='checkbox border-slate-900'
 						checked={selectedGender === "female"}
 						onChange={() => onCheckboxChange("female")}
@@ -27,24 +27,3 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 	);
 };
 export default GenderCheckbox;
-
-// STARTER CODE FOR THIS FILE
-// const GenderCheckbox = () => {
-// 	return (
-// 		<div className='flex'>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Male</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Female</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 		</div>
-// 	);
-// };
-// export default GenderCheckbox;
